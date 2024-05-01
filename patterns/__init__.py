7b7b6cd6ac8ced270600d918b25eabebadca995a
Aleksandr Suvorov <smartlegiondev@gmail.com>
2024-05-01 18:26:39 +0700

    Added project directory structure

---
+++ patterns/__init__.py
@@ -0,0 +1,7 @@
+# --------------------------------------------------------
+# Licensed under the terms of the BSD 3-Clause License
+# (see LICENSE for details).
+# Copyright © 2018-2024, A.A Suvorov
+# All rights reserved.
+# --------------------------------------------------------
+"""A collection of design patterns and idioms in Python (With tests!)."""
