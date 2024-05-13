139da1123a6da5390ee5f909e65db960c8fd8876
A.A Suvorov <smartlegiondev@gmail.com>
2024-05-13 07:55:34 +0700

    Added django singleton

---
+++ patterns/creational/django_singleton.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+
+class SingletonModel(models.Model):
+    class Meta:
+        abstract = True
+
+    def save(self, *args, **kwargs):
+        self.pk = 1
+        super(SingletonModel, self).save(*args, **kwargs)
+
+    @classmethod
+    def load(cls):
+        obj, created = cls.objects.get_or_create(pk=1)
+        return obj
